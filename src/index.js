6071eb1e34fc65c78dabac7a32237b81d1fd1f03
stst5821 <stst5821@gmail.com>
2021-04-10 18:19:03 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// firstNameとlastNameをつなげる関数
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+// ユーザーネーム指定
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+// formatName関数を呼び出して、変数elementに代入
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// アプリケーションのパフォーマンスを測定・分析できるらしい。https://bit.ly/CRA-vitals
 reportWebVitals();
